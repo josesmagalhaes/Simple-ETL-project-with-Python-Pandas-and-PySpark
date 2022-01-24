869304c0d43bb7ee235fff8c11b814960fa15b79
José Magalhães <joses.magalhaes@hotmail.com>
2022-01-23 23:43:53 -0300

    Desenvolvimento de função para converter tipo de dados em colunas

--- main.py
+++ main.py
@@ -25,3 +25,27 @@ path = "E:/scripts/training.1600000.processed.noemoticon.csv"
 df   = spark.read.format("csv")\
                   .schema(schema)\
                   .load(path)
+
+df = df.withColumn("day_week", df.date.substr(1,3))\
+    .withColumn("day", df.date.substr(9,2))\
+    .withColumn("month", df.date.substr(5,3))\
+    .withColumn("time", df.date.substr(12,8))\
+    .withColumn("year", df.date.substr(25,4))\
+    .drop("date")
+
+df.printSchema()
+
+def converterColuna(dataframe, nomes, novoTipo):
+    for nome in nomes: 
+        dataframe = dataframe.withColumn(nome, dataframe[nome].cast(novoTipo))
+    return dataframe 
+
+colunas_string = ['day_week','month']
+colunas_inteiro = ['day']
+colunas_time = ['time']
+
+df = converterColuna(df, colunas_string, StringType())
+df = converterColuna(df, colunas_inteiro, IntegerType())
+df = converterColuna(df, colunas_time, TimestampType())
+
+df.printSchema()
